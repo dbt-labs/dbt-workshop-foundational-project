f7913ae82ed71d04dd9cd71035c9ff98f5cca07a
richard white <richard.white@dbtlabsworkshop.com>
2025-10-20 18:07:41 +0000

    new model urgent

---
+++ models/urgent_orders.sql
@@ -0,0 +1,12 @@
+select
+    date_trunc('month', order_date) as order_month,
+    market_segment,
+    count(*) as urgent_order_count
+from
+    {{ ref('fct_orders') }}
+where
+    priority_code = '1-URGENT'
+group by
+    1, 2
+order by
+    1, 2
