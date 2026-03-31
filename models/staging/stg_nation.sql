6840745a2a447235d83ce32a40885fdc975dbded
paul field <paul.field+1773961053@dbtlabsworkshop.com>
2026-03-31 05:15:46 +0000

    updated stq_nation and stg_region

--- models/staging/stg_nation.sql
+++ models/staging/stg_nation.sql
@@ -1,20 +1,20 @@
-with 
+with
 
 source as (
 
-    select * from {{ source('tpch', 'nation') }}
+   select * from {{ source('tpch', 'nation') }}
 
 ),
 
 renamed as (
 
-    select
-        n_nationkey,
-        n_name,
-        n_regionkey,
-        n_comment
+   select
+       n_nationkey as nation_key,
+       n_name as nation,
+       n_regionkey as region_key,
+       n_comment as nation_comment
 
-    from source
+   from source
 
 )
 
