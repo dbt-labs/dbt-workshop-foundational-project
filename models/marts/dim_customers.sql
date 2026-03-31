75e233dadc634e8bdcb16c655048de82579bff0c
tamara orth <tamara.orth@dbtlabsworkshop.com>
2026-03-31 05:15:59 +0000

    created dim_customers

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,19 @@
+with
+    customer as (select * from {{ ref("stg_customers") }}),
+
+    nation as (select * from {{ ref("stg_nation") }}),
+
+    region as (select * from {{ ref("stg_tpch_region") }})
+
+select
+    customer.customer_key,
+    customer.name,
+    customer.address,
+    nation,
+    region,
+    customer.phone_number,
+    customer.account_balance,
+    customer.market_segment
+from customer
+inner join nation on customer.nation_key = nation.nation_key
+inner join region on nation.region_key = region.region_key
