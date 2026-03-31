156d5e9d293d704300b5f5d141d5426664979397
kerry booshand <kerry.booshand@dbtlabsworkshop.com>
2026-03-31 05:15:57 +0000

    name changes

--- models/staging/stg_region.sql
+++ models/staging/stg_region.sql
@@ -1,19 +1,20 @@
-with 
+
+with
 
 source as (
 
-    select * from {{ source('tpch', 'region') }}
+   select * from {{ source('tpch', 'region') }}
 
 ),
 
 renamed as (
 
-    select
-        r_regionkey,
-        r_name,
-        r_comment
+   select
+       r_regionkey as region_key,
+       r_name as region,
+       r_comment as region_comment
 
-    from source
+   from source
 
 )
 
