de3e1c0f9052d7f6a6008b6686484d03f0564858
shreya kumaran <shreya.kumaran@dbtlabsworkshop.com>
2026-04-09 21:40:13 +0000

    test

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -11,7 +11,7 @@ cleanup as (
         c_custkey as customer_key,
         c_name as name,
         c_address as address, 
-        c_nationkey as nation_key,
+        'a' as nation_key,
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
