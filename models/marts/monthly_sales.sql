96bab2eb5bfb72e7d099d34d1a966390eee6bb7a
nina clemson <nina.clemson@dbtlabsworkshop.com>
2026-04-14 04:59:24 +0000

    testing stuff

---
+++ models/marts/monthly_sales.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with orders as (
+
+    select * from {{ ref('fct_orders') }}
+
+),
+
+monthly_sales as (
+
+    select
+        cast(date_trunc('month', order_date) as date) as sales_month,
+        count(*) as order_count,
+        count(distinct customer_key) as customer_count,
+        sum(gross_item_sales_amount) as gross_sales_amount,
+        sum(item_discount_amount) as discount_amount,
+        sum(item_tax_amount) as tax_amount,
+        sum(net_item_sales_amount) as net_sales_amount
+    from orders
+    group by 1
+
+)
+
+select *
+from monthly_sales
+order by sales_month
