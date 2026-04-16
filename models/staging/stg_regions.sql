366cad2dfaade88a5dc11809e65b6cb133583ad7
shivaprasad kotekar <shivaprasad.kotekar@dbtlabsworkshop.com>
2026-04-16 03:42:27 +0000

    stable build

---
+++ models/staging/stg_regions.sql
@@ -0,0 +1,19 @@
+with source as (
+
+    select * from {{ source('tpch', 'region') }}
+
+),
+
+cleanup as (
+
+    select
+
+        r_regionkey as region_key,
+        r_name as name,
+        r_comment as comment
+
+    from source
+
+)
+
+select * from cleanup
