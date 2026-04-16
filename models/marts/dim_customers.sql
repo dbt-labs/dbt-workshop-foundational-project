c49fcd915603f235967a2ea1c54de8b2e91e4792
jenny groat <jenny.groat@dbtlabsworkshop.com>
2026-04-16 04:15:05 +0000

    cleanup

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,30 +1,24 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
-
-with customers as (
-
-    select *
-    from {{ ref('stg_customers') }}
-
+with customer as (
+    select * from {{ ref('stg_customers') }}
 ),
-
-final as (
-
-    select
-        customer_key,
-        name,
-        address,
-        nation_key,
-        phone_number,
-        account_balance,
-        market_segment,
-        comment
-    from customers
-
+nation as (
+    select * from {{ ref('stg_nations') }}
+),
+region as (
+    select * from {{ ref('stg_regions') }}
 )
 
-select *
-from final
+select
+    customer.customer_key,
+    customer.name,
+    customer.address,
+    nation.name as nation,
+    region.name as region,
+    customer.phone_number,
+    customer.account_balance,
+    customer.market_segment
+from customer
+inner join nation
+    on customer.nation_key = nation.nation_key
+inner join region
+    on nation.region_key = region.region_key
