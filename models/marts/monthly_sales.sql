db132159ef3fec1ae3cf1b3e34b78f4b841eb589
neeraja chekka <neeraja.chekka+1776304275@dbtlabsworkshop.com>
2026-04-16 03:45:23 +0000

    dbt hands-on

---
+++ models/marts/monthly_sales.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with orders as (
+
+    select * from {{ ref('fct_orders') }}
+
+),
+
+final as (
+
+    select
+        date_trunc('month', order_date) as month_start,
+        count(*) as order_count,
+        sum(gross_item_sales_amount) as gross_sales_amount,
+        sum(item_discount_amount) as discount_amount,
+        sum(item_tax_amount) as tax_amount,
+        sum(net_item_sales_amount) as net_sales_amount,
+        avg(net_item_sales_amount) as avg_order_net_sales_amount
+    from orders
+    group by 1
+
+)
+
+select *
+from final
+order by month_start
