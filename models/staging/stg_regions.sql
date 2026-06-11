3a532763e47c425732c030719ccd8e78958dfc51
bruno tran <bruno.tran+1781156345@dbtlabsworkshop.com>
2026-06-11 07:19:10 +0000

    create dim_customers, and related staging tables

---
+++ models/staging/stg_regions.sql
@@ -0,0 +1,20 @@
+with
+
+source as (
+
+   select * from {{ source('tpch', 'region') }}
+
+),
+
+renamed as (
+
+   select
+       r_regionkey as region_key,
+       r_name as region,
+       r_comment as region_comment
+
+   from source
+
+)
+
+select * from renamed
